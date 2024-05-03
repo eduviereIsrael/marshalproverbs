e3d7de2c19be3e296cd8adb353bcf9035d990e82
eduviereisrael <eduviereisrael@gmail.com>
2024-05-03 19:19:27 +0100

    fast tracked

--- components/poemCard/planTag.jsx
+++ components/poemCard/planTag.jsx
@@ -5,16 +5,16 @@ const PlanTag = ({plan}) => {
     const getBg = (tagPlan) => {
         switch(tagPlan.toLowerCase()){
             case 'gold':
-                return '#4F6C70'
+                return '#8d99ae'
             case 'platinum' :
                 return '#AF9070'
             default:
-                return '#D064D2'
+                return '#656d4a'
         }
     }
 
   return (
-    <div className='tag' style={{backgroundColor: getBg(plan), width: "max-content", fontSize: '11px', fontWeight: '400', padding: '2px 11px', borderRadius: '30px'}} >{plan}</div>
+    <div className='tag' style={{backgroundColor: getBg(plan), color: '#fff', width: "max-content", fontSize: '11px', fontWeight: '400', padding: '2px 11px', borderRadius: '30px'}} >{plan}</div>
   )
 }
 
